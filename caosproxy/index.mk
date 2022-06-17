68c51775f79875c6382c0ea0f507bd4ee37ef4a0
20kdc <asdd2808@gmail.com>
2022-06-17 18:07:19 +0100

    fix really silly fail

--- caosproxy/index.mk
+++ caosproxy/index.mk
@@ -4,7 +4,7 @@ rel: bin/caosprox.exe caosproxy/spec.txt caosproxy/caosterm.py
 INTERMEDIATES += bin/caosprox.exe bin/caospres.o
 
 bin/caosprox.exe: caosproxy/cpxservg.c caosproxy/cpxservi.c bin/caospres.o
-	$(COMPILER) $(COMPILE_FLAGS_DLL) $^ -o $@ -lws2_32
+	$(COMPILER) $(COMPILE_FLAGS_GUI) $^ -o $@ -lws2_32
 	$(STRIP) $@
 
 bin/caospres.o: caosproxy/caospres.rc caosproxy/caosprox.ico
