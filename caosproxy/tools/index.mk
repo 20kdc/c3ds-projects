6eb2f470e31e4ed076267492ae54ee3df71d27c2
20kdc <asdd2808@gmail.com>
2022-06-18 13:25:48 +0100

    Add missing libcpx.py to release

--- caosproxy/tools/index.mk
+++ caosproxy/tools/index.mk
@@ -1,2 +1,2 @@
-rel: caosproxy/tools/caosterm.py caosproxy/tools/maprooms.py caosproxy/tools/maptypes.py
+rel: caosproxy/tools/caosterm.py caosproxy/tools/libcpx.py caosproxy/tools/maprooms.py caosproxy/tools/maptypes.py
 
