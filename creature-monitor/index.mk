112d70af1837499e70c9b891536839d9ec2db8e3
20kdc <asdd2808@gmail.com>
2022-08-19 16:59:02 +0100

    creature-monitor will be "C++ as better C" with no C++ stdlib, I think

--- creature-monitor/index.mk
+++ creature-monitor/index.mk
@@ -4,8 +4,8 @@ all: creature-monitor/creature-monitor
 rel: creature-monitor/creature-monitor
 INTERMEDIATES += creature-monitor/creature-monitor
 
-creature-monitor/creature-monitor: creature-monitor/main.c creature-monitor/page-elf64.o
-	$(CC) creature-monitor/main.c creature-monitor/page-elf64.o -lSDL2 -lSDL2_net -o $@
+creature-monitor/creature-monitor: creature-monitor/main.cpp creature-monitor/page-elf64.o
+	$(CC) -fno-rtti -fno-exceptions creature-monitor/main.cpp creature-monitor/page-elf64.o -lSDL2 -lSDL2_net -o $@
 
 endif
 
@@ -15,8 +15,8 @@ all: creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-mo
 rel: creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-monitor/SDL2_net.dll
 INTERMEDIATES += creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-monitor/SDL2_net.dll
 
-creature-monitor/creature-monitor.exe: creature-monitor/main.c creature-monitor/page-coff.o
-	$(W32_CC) $(W32_CFLAGS_SDL) creature-monitor/main.c creature-monitor/page-coff.o -o $@ -lSDL2 -lSDL2_net
+creature-monitor/creature-monitor.exe: creature-monitor/main.cpp creature-monitor/page-coff.o
+	$(W32_CC) -fno-rtti -fno-exceptions $(W32_CFLAGS_SDL) creature-monitor/main.cpp creature-monitor/page-coff.o -o $@ -lSDL2 -lSDL2_net
 
 creature-monitor/SDL2.dll:
 	cp /usr/local/i686-w64-mingw32/bin/SDL2.dll $@ || cp /usr/i686-w64-mingw32/bin/SDL2.dll $@ || cp /mingw32/bin/SDL2.dll $@
---
+++ creature-monitor/main.cpp
@@ -0,0 +1,70 @@
+/*
+ * creature-monitor - watching norns from afar?
+ * Written starting in 2022 by 20kdc
+ * To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+ * You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+
+#include <stdio.h>
+
+extern const char cm_page_start[];
+extern const int cm_page_len;
+
+SDL_Window * gWindow;
+SDL_Renderer * gRenderer;
+SDL_Texture * gFont;
+
+void errorOut(const char * reason) {
+	puts(reason);
+	SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Error!", reason, gWindow);
+	exit(1);
+}
+
+void writeText(int x, int y, const char * text) {
+	SDL_Rect src = {0, 0, 7, 14};
+	SDL_Rect dst = {x, y, 7, 14};
+	while (*text) {
+		char ch = *text++;
+		if (ch == 10) {
+			dst.x = x;
+			dst.y += 16;
+		} else {
+			src.x = (ch & 0x0F) * 7;
+			src.y = ((ch & 0xF0) >> 4) * 14;
+			SDL_RenderCopy(gRenderer, gFont, &src, &dst);
+			dst.x += 8;
+		}
+	}
+}
+
+int main(int argc, char ** argv) {
+	SDL_Init(SDL_INIT_VIDEO);
+	if (SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &gWindow, &gRenderer))
+		errorOut("wah! failed to create window/renderer!");
+	SDL_Surface * s = SDL_LoadBMP_RW(SDL_RWFromConstMem(cm_page_start, cm_page_len), 1);
+	if (!s)
+		errorOut("wah! failed to open BMP!");
+	puts("opened BMP");
+	gFont = SDL_CreateTextureFromSurface(gRenderer, s);
+	if (!gFont)
+		errorOut("wah! failed to create texture!");
+	puts("created texture");
+	while (1) {
+		SDL_Event ev;
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT)
+				return 0;
+		}
+		SDL_Delay(100);
+		SDL_SetRenderDrawColor(gRenderer, 0, 0, 0, 255);
+		SDL_RenderClear(gRenderer);
+		SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+		writeText(4, 4, "Hello world!");
+
+		SDL_RenderPresent(gRenderer);
+	}
+}
+
