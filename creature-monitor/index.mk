81c9238beaaf194e6d8a416ff7142800e47ab951
20kdc <asdd2808@gmail.com>
2022-08-19 16:40:14 +0100

    "compat test" version of the funny little project

--- creature-monitor/index.mk
+++ creature-monitor/index.mk
@@ -4,8 +4,8 @@ all: creature-monitor/creature-monitor
 rel: creature-monitor/creature-monitor
 INTERMEDIATES += creature-monitor/creature-monitor
 
-creature-monitor/creature-monitor: creature-monitor/main.c
-	$(CC) creature-monitor/main.c -lSDL2 -lSDL2_net -o $@
+creature-monitor/creature-monitor: creature-monitor/main.c creature-monitor/page-elf64.o
+	$(CC) creature-monitor/main.c creature-monitor/page-elf64.o -lSDL2 -lSDL2_net -o $@
 
 endif
 
@@ -15,8 +15,8 @@ all: creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-mo
 rel: creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-monitor/SDL2_net.dll
 INTERMEDIATES += creature-monitor/creature-monitor.exe creature-monitor/SDL2.dll creature-monitor/SDL2_net.dll
 
-creature-monitor/creature-monitor.exe: creature-monitor/main.c
-	$(W32_CC) $(W32_CFLAGS_SDL) creature-monitor/main.c -o $@ -lSDL2 -lSDL2_net
+creature-monitor/creature-monitor.exe: creature-monitor/main.c creature-monitor/page-coff.o
+	$(W32_CC) $(W32_CFLAGS_SDL) creature-monitor/main.c creature-monitor/page-coff.o -o $@ -lSDL2 -lSDL2_net
 
 creature-monitor/SDL2.dll:
 	cp /usr/local/i686-w64-mingw32/bin/SDL2.dll $@ || cp /usr/i686-w64-mingw32/bin/SDL2.dll $@ || cp /mingw32/bin/SDL2.dll $@
@@ -25,4 +25,10 @@ creature-monitor/SDL2_net.dll:
 	cp /usr/local/i686-w64-mingw32/bin/SDL2_net.dll $@ || cp /usr/i686-w64-mingw32/bin/SDL2_net.dll $@ || cp /mingw32/bin/SDL2_net.dll $@
 
 endif
+
+INTERMEDIATES += creature-monitor/page-elf64.o creature-monitor/page-coff.o
+creature-monitor/page-elf64.o: creature-monitor/page.asm creature-monitor/page.bmp
+	$(NASM) -felf64 $< -o $@
+creature-monitor/page-coff.o: creature-monitor/page.asm creature-monitor/page.bmp
+	$(NASM) -fwin32 $< -o $@
 
--- creature-monitor/main.c
+++ creature-monitor/main.c
@@ -8,11 +8,24 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
+#include <stdio.h>
+
+extern const char cm_page_start[];
+extern const int cm_page_len;
+
 int main(int argc, char ** argv) {
 	SDL_Window * w;
 	SDL_Renderer * r;
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(640, 480, SDL_WINDOW_RESIZABLE, &w, &r);
+	SDL_Surface * s = SDL_LoadBMP_RW(SDL_RWFromConstMem(cm_page_start, cm_page_len), 1);
+	if (!s)
+		puts("wah! failed to open BMP!");
+	puts("opened BMP");
+	SDL_Texture * t = SDL_CreateTextureFromSurface(r, s);
+	if (!t)
+		puts("wah! failed to create texture!");
+	puts("created texture");
 	while (1) {
 		SDL_Event ev;
 		while (SDL_PollEvent(&ev)) {
@@ -33,6 +46,8 @@ int main(int argc, char ** argv) {
 
 		SDL_RenderDrawLine(r, 256, 400, 300, 80);
 		SDL_RenderDrawLine(r, 256, 160, 400, 160);
+
+		SDL_RenderCopy(r, t, NULL, NULL);
 
 		SDL_RenderPresent(r);
 	}
