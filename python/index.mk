52f92f6c624f9636a8cb9abb0b1ddb0caee49e7e
20kdc <asdd2808@gmail.com>
2023-09-05 10:18:45 +0100

    Easier install for the blender addon

--- python/index.mk
+++ python/index.mk
@@ -1,17 +1,22 @@
+LIBKC3DS_PY += libkc3ds/__init__.py
+LIBKC3DS_PY += libkc3ds/s16.py
+LIBKC3DS_PY += libkc3ds/s16pil.py
+LIBKC3DS_PY += libkc3ds/cpx.py
+LIBKC3DS_PY += libkc3ds/bitdither.py
+LIBKC3DS_PY += libkc3ds/parts.py
+LIBKC3DS_ABS = $(addprefix python/,$(LIBKC3DS_PY))
+
+KC3DSBPY_PY += kc3dsbpy/__init__.py
+KC3DSBPY_PY += kc3dsbpy/imaging.py
+KC3DSBPY_PY += kc3dsbpy/database.py
+KC3DSBPY_PY += kc3dsbpy/dataext.py
+KC3DSBPY_PY += kc3dsbpy/gizmo.py
+KC3DSBPY_PY += kc3dsbpy/framereq.py
+KC3DSBPY_ABS = $(addprefix python/,$(KC3DSBPY_PY))
+
 PYTHON_REL += python/README.md
 
-PYTHON_REL += python/libkc3ds/s16.py
-PYTHON_REL += python/libkc3ds/s16pil.py
-PYTHON_REL += python/libkc3ds/cpx.py
-PYTHON_REL += python/libkc3ds/bitdither.py
-PYTHON_REL += python/libkc3ds/parts.py
-
-PYTHON_REL += python/kc3dsbpy/__init__.py
-PYTHON_REL += python/kc3dsbpy/imaging.py
-PYTHON_REL += python/kc3dsbpy/database.py
-PYTHON_REL += python/kc3dsbpy/dataext.py
-PYTHON_REL += python/kc3dsbpy/gizmo.py
-PYTHON_REL += python/kc3dsbpy/framereq.py
+PYTHON_REL += $(LIBKC3DS_ABS)
 
 PYTHON_REL += python/caosterm.py python/cpxinfo.py python/inject.py
 PYTHON_REL += python/bdmptest.py python/maprooms.py python/maptypes.py
@@ -21,6 +26,13 @@ PYTHON_REL += python/s16tool.py
 PYTHON_REL += python/c3breedie.py
 
 PYTHON_REL += python/cpxciesv.py
+
+INTERMEDIATES += python/blender-addons.zip
+all: python/blender-addons.zip
+
+python/blender-addons.zip: $(LIBKC3DS_ABS) $(KC3DSBPY_ABS)
+	rm -f $@
+	cd python && zip -r -n .pyc blender-addons.zip $(LIBKC3DS_PY) $(KC3DSBPY_PY)
 
 rel: $(PYTHON_REL)
 rel-sdk: $(PYTHON_REL)
---
+++ python/libkc3ds/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Exists so Blender can install the addon
+
+# libkc3ds - General-purpose Python library for C3/DS
+# Written starting in 2023 by contributors (see CREDITS.txt)
+# To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
+# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+bl_info = {
+	"name": "General-purpose Python library for C3/DS",
+	"version": (0, 1, 0),
+	"blender": (3, 6, 0),
+	"category": "Libraries",
+	"description": "Used by kc3dsbpy. Be sure to also enable the actual Blender addon!"
+}
+
+def register():
+	pass
+
+def unregister():
+	pass
+
