e002090a1a892b8f41713547e5e3d6621168c226
20kdc <asdd2808@gmail.com>
2024-03-16 17:16:46 +0000

    Aquarium: SYLLABLE STRUCTURE IS HARD

--- python/cvnstudy.py
+++ python/cvnstudy.py
@@ -6,7 +6,7 @@
 # You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 # Just exists to figure out some stuff don't mind me
-syllable_types = ["V", "CV", "VN", "CVN"]
+syllable_types = ["", "V", "CV", "VN", "CVN"]
 
 done = {}
 
